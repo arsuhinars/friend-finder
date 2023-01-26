b14cc39c1a93818589cad880433a44c8fbe51783
arsuhinars <arsuhinars@gmail.com>
2023-01-26 20:58:15 +0400

    Added entrypoint for backend image

---
+++ backend/docker-entrypoint.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -e
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+# Read secrets
+file_env "REDIS_URL"
+file_env "DATABASE_URL"
+
+# Start main app with giver parameters
+uvicorn app.main:app --port $BACKEND_PORT --host 0.0.0.0 $@
