247ba6862b9344aa251445007bff13e3a04ce29d
arsuhinars <arsuhinars@gmail.com>
2023-01-27 19:41:48 +0400

    Added Redis connection pool

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+import redis.asyncio as redis
+
+from .settings import settings
 
 app = FastAPI()
+
+@app.on_event('startup')
+async def on_start():
+    app.state.redis_pool = redis.ConnectionPool.from_url(settings.redis_url)
+
+
+@app.on_event('shutdown')
+async def on_shutdown():
+    await app.state.redis_pool.disconnect()
 
 
 # Include API routers
