a9ace6625fc1219610fb208975c1bfa56311419c
arsuhinars <arsuhinars@gmail.com>
2023-01-31 14:47:07 +0400

    ✨ Added dependencies for db session and redis client

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -1,6 +1,12 @@
+from functools import lru_cache
+from typing import AsyncIterable
+
+from sqlalchemy.ext.asyncio import AsyncSession
 from redis.asyncio import Redis
 
 from app.settings import AppSettings
+from app.redis import get_redis_pool
+from app.db import get_session_maker
 
 
 @lru_cache
@@ -8,9 +14,15 @@ def get_settings():
     return AppSettings()
 
 
-async def get_redis_client() -> Redis:
+async def get_db_session() -> AsyncIterable[AsyncSession]:
+    session_maker = get_session_maker()
+    with session_maker() as session:
+        yield session
+
+
+async def get_redis_client() -> AsyncIterable[Redis]:
     try:
-        redis = Redis(connection_pool=app.state.redis_pool)
+        redis = Redis(connection_pool=get_redis_pool())
         yield redis
     finally:
         await redis.close()
