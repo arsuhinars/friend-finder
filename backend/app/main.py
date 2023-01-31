98ee5446e318c404ffa14ef14d65c2d336f53714
arsuhinars <arsuhinars@gmail.com>
2023-01-31 14:45:09 +0400

    🎨 Changed db and redis initialization behaviour

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,21 +1,18 @@
 from fastapi import FastAPI
-import redis.asyncio as redis
-
-from .settings import settings
 
 app = FastAPI()
 
+from app.db import initialize_db, finalize_db
+from app.redis import initialize_redis, finalize_redis
+
+
 @app.on_event('startup')
 async def on_start():
-    app.state.redis_pool = redis.ConnectionPool.from_url(settings.redis_url)
+    await initialize_db()
+    await initialize_redis()
 
 
 @app.on_event('shutdown')
 async def on_shutdown():
-    await app.state.redis_pool.disconnect()
-
-
-# Include API routers
-from .routers import router
-
-app.include_router(router)
+    await finalize_db()
+    await finalize_redis()
